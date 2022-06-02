2ce9355071ed548c8120792b6778813de1f16e0f
Roger <rogerjohan.r@gmail.com>
2022-06-02 18:14:42 +0530

    Updated Gecos

--- genUser.sh
+++ genUser.sh
@@ -12,7 +12,7 @@ if [ $# -eq 0 ]
 	read agetype
 	echo "Legacy account?(legacy,-):"
 	read legacy
-	echo | awk -v a="$accno" -v b="$branch" -v r="$restype" -v age="$agetype" -v legacy="$legacy" '{print "ACC"a":ACC"a":::"b","r","age","legacy":/home/ACC"a":/bin/bash"}' > accounts.txt
+	echo | awk -v a="$accno" -v b="$branch" -v r="$restype" -v age="$agetype" -v legacy="$legacy" '{print "ACC"a":ACC"a":::ACC"a","b","r","age","legacy":/home/ACC"a":/bin/bash"}' > accounts.txt
 	newusers accounts.txt
 	account=$(cut -d: -f1 accounts.txt | cat)
 	sudo touch "/home/$account/Current_Balance.txt"
@@ -23,7 +23,7 @@ if [ $# -eq 0 ]
 fi
 
 
-awk -F' ' '{print $1":"$1":::"$2","$3","$4","$5":/home/"$1":/bin/bash"}' $1 > accounts.txt
+awk -F' ' '{print $1":"$1":::"$1","$2","$3","$4","$5":/home/"$1":/bin/bash"}' $1 > accounts.txt
 
 for account in $(cut -d: -f1 accounts.txt | cat )
 do
