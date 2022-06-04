b8c5f27fb333fbbcf3d6026425c53cb0755ec397
Roger <rogerjohan.r@gmail.com>
2022-06-05 03:02:52 +0530

    files and .bashrc

---
+++ bashrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "alias genUser='genUser.sh'" >> ~/.bashrc
+echo "alias permit='permit.sh'" >> ~/.bashrc
+echo "alias updateBranch='updateBranch.sh'" >> ~/.bashrc
+echo "alias allotInterest='allotInterest.sh'" >> ~/.bashrc
+echo "alias makeTransaction='makeTransaction.sh'" >> ~/.bashrc
