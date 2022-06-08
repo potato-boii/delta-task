941cc0764abe5ad64a7843e3fdb5813f81634b0a
Roger <rogerjohan.r@gmail.com>
2022-06-09 01:19:22 +0530

    Fixed errors

--- allotInterest.sh
+++ allotInterest.sh
@@ -11,7 +11,7 @@ grep "Branch$number" /etc/passwd | cut -d: -f1 > accounts.txt
 
 
 
-for account in $(cat accounts.txt)
+for account in $(cat /home/$USER/accounts.txt)
 do 	
 	balance=0
 	current_balance=$(cat /home/$account/Current_Balance.txt)
@@ -20,42 +20,42 @@ do
 	Legacy=$(grep $account /etc/passwd | awk -F ":" '{print $5}' | cut -d, -f5 )
 	if [[ $agetype = "minor" ]]
 	then
-		rate=$(grep $agetype Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $agetype /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
 	fi
 	if [[ $agetype = "seniorCitizen" ]]
 	then
-		rate=$(grep $agetype Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $agetype /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
 	fi
 	if [[ $restype = "foreigner" ]]
 	then
-		rate=$(grep $restype Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $restype /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
 	fi
 	if [[ $restype = "resident" ]]
 	then
-		rate=$(grep $restype Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $restype /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
 	fi
 	if [[ $restype = "citizen" ]]
 	then
-		rate=$(grep $restype Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $restype /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
 	fi
 	if [[ $Legacy = "legacy" ]]
 	then
-		rate=$(grep $Legacy Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
+		rate=$(grep $Legacy /home/$USER/Daily_Interest_Rates.txt | cut -d ' ' -f2 | cut -d% -f1 | cat )
 		rate=$(echo "scale=10;($rate * 0.01)/1" | bc )
 		interest=$(echo "scale=10;($rate * $current_balance)/1" | bc )
 		balance=$(echo "scale=10;($interest + $balance)/1" | bc )
--- genUser.sh
+++ genUser.sh
@@ -15,27 +15,27 @@ if [ $# -eq 0 ]
 	echo | awk -v a="$accno" -v b="$branch" -v r="$restype" -v age="$agetype" -v legacy="$legacy" '{print "ACC"a":ACC"a":::ACC"a","b","r","age","legacy":/home/ACC"a":/bin/bash"}' > accounts.txt
 	newusers accounts.txt
 	account=$(cut -d: -f1 accounts.txt | cat)
-	sudo touch "/home/$account/Current_Balance.txt"
-	sudo echo 500 > "/home/$account/Current_Balance.txt"
-	sudo touch "/home/$account/Transaction_History.txt"
+	touch "/home/$account/Current_Balance.txt"
+	echo 500 > "/home/$account/Current_Balance.txt"
+	touch "/home/$account/Transaction_History.txt"
 	rm accounts.txt
 	exit 0
 fi
 
 
 awk -F' ' '{print $1":"$1":::"$1","$2","$3","$4","$5":/home/"$1":/bin/bash"}' $1 > accounts.txt
-
-for account in $(cut -d: -f1 accounts.txt | cat )
-do
-	sudo touch "/home/$account/Current_Balance.txt"
-	sudo echo 500 > "/home/$account/Current_Balance.txt"
-	sudo touch "/home/$account/Transaction_History.txt"
-done
-
 cut -d ' ' -f 2 $1 | sort | uniq | sed 's/Branch/MANAGER/' | awk '{print $1":"$1"::::/home/"$1":/bin/bash"}' > manager.txt
 
 newusers accounts.txt
 newusers manager.txt
+
+for account in $(cut -d: -f1 accounts.txt | cat )
+do
+	touch "/home/$account/Current_Balance.txt"
+	echo 500 > "/home/$account/Current_Balance.txt"
+	touch "/home/$account/Transaction_History.txt"
+done
+
 
 rm accounts.txt
 rm manager.txt
